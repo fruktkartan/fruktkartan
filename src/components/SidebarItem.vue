0a975c038d32e36107e50f07c67d4f94306a732e
rotsee <mejl@leowallentin.se>
2020-02-08 15:50:06 +0100

    use custom component for menu sidebar items

---
+++ src/components/SidebarItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-list-item @click="$emit('onAction')">
+    <v-list-item-icon>
+      <v-tooltip left>
+        <template v-slot:activator="{on}">
+        <v-icon v-on="on">mdi-{{ icon }}</v-icon>
+      </template>
+        <span>{{ tooltip }}</span>
+      </v-tooltip>
+    </v-list-item-icon>
+    <v-list-item-content>{{ text }}</v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+
+export default {
+  name: "SidebarItem",
+  props: ["text", "tooltip", "icon"]
+}
+</script>
