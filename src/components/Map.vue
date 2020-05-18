975d62ca83f072e19e7bb9e44e8ddbf623fd1fb0
rotsee <mejl@leowallentin.se>
2020-05-19 00:01:10 +0200

    raise menu button a bit more

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -21,7 +21,7 @@
       <l-control
         position="bottomleft"
         class="hidden-md-and-up control"
-        style="z-index: 1; position: absolute; display: flex; bottom: 2em;"
+        style="z-index: 1; position: absolute; display: flex; bottom: 2.5em;"
       >
         <v-icon alt="Meny" title="Meny" @click="$emit('openDrawer')">
           {{ mdiMenu }}
