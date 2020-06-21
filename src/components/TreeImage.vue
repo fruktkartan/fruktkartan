72cade85d561dad3ec19c74a54d3797aaeb3e9d5
rotsee <mejl@leowallentin.se>
2020-06-21 13:25:05 +0200

    add placeholder for loading images; poll for image five times before showing error msg, in case it was just uploaded; Fix a bug where warning message wouldnt go away when opening a new tree; Put warning message in v-card for consistent style. This fixes #24

--- src/components/TreeImage.vue
+++ src/components/TreeImage.vue
@@ -2,20 +2,27 @@
 <template>
   <div>
     <v-img
-      v-if="image"
+      v-if="image && !err"
       :src="img"
+      :lazy-src="placeholder"
       :srcset="img_srcset"
       :alt="alt"
       :width="width"
-      @error="
-        image = false
-        err = true
-      "
-    />
-    <div v-if="err" class="warning">
-      &lt; Bilden kunde inte visas just nu. Det kan bero på att den nyss laddats
-      upp, och fortfarande bearbetas. &gt;
-    </div>
+      @error="startPolling()"
+      @load="stopPolling()"
+    >
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="grey lighten-1" />
+        </v-row>
+      </template>
+    </v-img>
+    <v-card v-if="err" class="warning">
+      <v-card-text>
+        Bilden kunde inte visas just nu. Det kan bero på att den nyss laddats
+        upp och fortfarande bearbetas, eller på att den är trasig.
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -35,20 +42,64 @@ export default {
       default: "",
     },
   },
-  data() {
+  data: function () {
     return {
       err: false,
       width: WIDTH,
+      placeholder: require("@/assets/placeholder.jpg"),
+      timer: null,
+      timerTicks: 0,
     }
+  },
+  watch: {
+    image: function () {
+      // Because the same component is reused, we need to reset
+      // some data when showing a new image
+      this.err = false
+      this.stopPolling()
+      this.timerTicks = 0
+      this.timer = null
+    },
   },
   computed: {
     img: function () {
-      return `${S3_BASE}/${this.image}_${WIDTH}.jpg`
+      // Adding the url fragment serves two purposes:
+      // - It will trigger a redraw (polling for the image)
+      // - It will fix some cache issues when an image is generated after page load
+      return `${S3_BASE}/${this.image}_${WIDTH}.jpg?ci=${this.timerTicks}`
     },
     img_srcset: function () {
       return [1, 1.5, 2, 3]
         .map(x => `${S3_BASE}/${this.image}_${WIDTH * x}.jpg ${x}x`)
         .join(",")
+    },
+  },
+  beforeDestroy() {
+    // This is probably never really needed
+    this.stopPolling()
+  },
+  methods: {
+    stopPolling: function () {
+      if (this.timer) {
+        clearInterval(this.timer)
+      }
+    },
+    startPolling: function () {
+      // start polling the image server for an image, in case it wasn't
+      // yet generated on the first attemt. Generating an image thumb can
+      // sometimes take a couple of seconds.
+
+      if (this.timer) {
+        // Don't start multiple timers
+        return
+      }
+      this.timer = setInterval(() => {
+        this.timerTicks += 1
+        if (this.timerTicks > 5) {
+          this.err = true
+          this.stopPolling()
+        }
+      }, 750)
     },
   },
 }
