37b1f1231f03e8104e83b6541b635881e5f48f30
rotsee <mejl@leowallentin.se>
2021-01-02 15:17:04 +0100

    var -> let

--- src/components/ViewTreeDialog.vue
+++ src/components/ViewTreeDialog.vue
@@ -87,7 +87,7 @@ function raiseOnHttpError(response) {
   if (response.status >= 200 && response.status < 300) {
     return response
   } else {
-    var error = new Error(response.statusText)
+    let error = new Error(response.statusText)
     error.response = response
     throw error
   }
