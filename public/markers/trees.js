111c5ab16c69faf490aad20f900b044d5b9081e6
rotsee <leo_wallentin@hotmail.com>
2024-11-03 21:17:21 +0100

    skip Vite

---
+++ public/markers/trees.js
@@ -0,0 +1,78 @@
+import berry from './berry.svg'
+import berryBlackberry from './berry-blackberry.svg'
+import berryBackcurrant from './berry-blackcurrant.svg'
+import berryChokeberry from './berry-chokeberry.svg'
+import berryCurrant from './berry-currant.svg'
+import berryElder from './berry-elder.svg'
+import berryGoji from './berry-goji.svg'
+import berryMulberry from './berry-mullberry.svg'
+import berryRaspberry from './berry-raspberry.svg'
+import berryRosehip from './berry-rosehip.svg'
+import berryRowanberry from './berry-rowanberry.svg'
+import berrySeabuckthorn from './berry-seabuckthorn.svg'
+import berrySloeberry from './berry-sloeberry.svg'
+import berryWildstrawberry from './berry-wildstrawberry.svg'
+import herbChamomille from './herb-chamomille.svg'
+import nut from './nut.svg'
+import nutWalnut from './nut-walnut.svg'
+import nutHazelnut from './nut-hazelnut.svg'
+import nutSweetchestnut from './nut-sweetchestnut.svg'
+import tree from './tree.svg'
+import treeApple from './tree-apple.svg'
+import treeCherry from './tree-cherry.svg'
+import treePear from './tree-pear.svg'
+import treePlum from './tree-plum.svg'
+import treeQuince from './tree-quince.svg'
+import treeRhubarb from './tree-rhubarb.svg'
+
+export default {
+  berry,
+  berryBlackberry,
+  berryBackcurrant,
+  berryChokeberry,
+  berryCurrant,
+  berryElder,
+  berryGoji,
+  berryMulberry,
+  berryRaspberry,
+  berryRosehip,
+  berryRowanberry,
+  berrySeabuckthorn,
+  berrySloeberry,
+  berryWildstrawberry,
+  herbChamomille,
+  nut,
+  nutWalnut,
+  nutHazelnut,
+  nutSweetchestnut,
+  tree,
+  treeApple,
+  treeCherry,
+  treePear,
+  treePlum,
+  treeQuince,
+  treeRhubarb,
+  "berry-blackberry": berryBlackberry,
+  "berry-blackcurrant": berryBackcurrant,
+  "berry-chokeberry": berryChokeberry,
+  "berry-currant": berryCurrant,
+  "berry-elder": berryElder,
+  "berry-goji": berryGoji,
+  "berry-mulberry": berryMulberry,
+  "berry-raspberry": berryRaspberry,
+  "berry-rosehip": berryRosehip,
+  "berry-rowanberry": berryRowanberry,
+  "berry-seabuckthorn": berrySeabuckthorn,
+  "berry-sloeberry": berrySloeberry,
+  "berry-wildstrawberry": berryWildstrawberry,
+  "herb-chamomille": herbChamomille,
+  "nut-walnut": nutWalnut,
+  "nut-hazelnut": nutHazelnut,
+  "nut-sweetchestnut": nutSweetchestnut,
+  "tree-apple": treeApple,
+  "tree-cherry": treeCherry,
+  "tree-pear": treePear,
+  "tree-plum": treePlum,
+  "tree-quince": treeQuince,
+  "tree-rhubarb": treeRhubarb,
+}
