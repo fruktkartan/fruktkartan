a1f029e8b213314f505966a16c224287ac86090b
Daniel Lublin <daniel@lublin.se>
2025-05-20 09:06:33 +0200

    Fix __dirname (lint)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from "@vitejs/plugin-vue"
 import vuetify from "vite-plugin-vuetify"
 import svgLoader from "vite-svg-loader"
 import { resolve } from "path"
+import path from "path"
+import { fileURLToPath } from "url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +15,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
     },
   },
 })
